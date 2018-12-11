3e9927a61f5bb43c011f5b6c1ebea512485396a1
Fatih Toker <fatihtoker@anadolu.edu.tr>
2018-12-11 11:55:20 +0300

    basic grid system and cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {Observable} from 'rxjs';
-import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +6,4 @@ import {AngularFirestore} from '@angular/fire/firestore';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  products: Observable<any[]>;
-  data: any;
-  constructor(private db: AngularFirestore) {
-    this.products = db.collection('products').valueChanges();
-    this.products.subscribe(data => {
-      console.log(data);
-    });
-  }
 }
--- src/app/site/home/home.component.ts
+++ src/app/site/home/home.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Observable} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  products: Observable<any[]>;
+  data: any;
+  constructor(private db: AngularFirestore) {
+    this.products = db.collection('products').valueChanges();
+    this.products.subscribe(data => {
+      this.data = data;
+    });
   }
-
 }
